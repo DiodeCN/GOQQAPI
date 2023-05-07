43c148dcd3eab5d106381982e2a7c10f2475c8a5
DiodeCN <1069523953@qq.com>
2023-05-07 22:22:25 +0800

    不要这个

--- main.go
+++ main.go
@@ -73,23 +73,3 @@ func main() {
 		return
 	}
 }
-
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	// 将HTTP连接升级为WebSocket
-	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
-	if err != nil {
-		fmt.Println("升级WebSocket连接失败：", err)
-		return
-	}
-	defer conn.Close()
-
-	// 无限循环，读取传入的WebSocket消息
-	for {
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println("读取消息出错：", err)
-			return
-		}
-		fmt.Printf("收到消息：%s\n", message)
-	}
-}
