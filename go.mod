9535e60fe2439a4ccb7a05d5899af85e6018b243
DiodeCN <1069523953@qq.com>
2023-05-07 21:54:29 +0800

    非常抱歉，我误解了您的问题。以下是一个简单的Go程序，可以接收传入的WebSocket消息：

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/DiodeCN/GOQQAPI
 
 go 1.18
+
+require github.com/gorilla/websocket v1.5.0 // indirect
--- main.go
+++ main.go
@@ -1,31 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/url"
+	"fmt"
+	"net/http"
 
-    "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 )
 
 func main() {
-    // WebSocket服务器的URL
-    url := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/websocket"}
+	http.HandleFunc("/", handleWebSocket)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("启动Web服务失败：", err)
+		return
+	}
+}
 
-    // 连接到WebSocket服务器
-    conn, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
-    if err != nil {
-        log.Fatal("无法连接到WebSocket服务器：", err)
-    }
-    defer conn.Close()
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// 将HTTP连接升级为WebSocket
+	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if err != nil {
+		fmt.Println("升级WebSocket连接失败：", err)
+		return
+	}
+	defer conn.Close()
 
-    // 接收来自WebSocket服务器的消息
-    for {
-        _, message, err := conn.ReadMessage()
-        if err != nil {
-            log.Println("读取消息出错：", err)
-            return
-        }
-        fmt.Printf("收到消息：%s\n", message)
-    }
+	// 无限循环，读取传入的WebSocket消息
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("读取消息出错：", err)
+			return
+		}
+		fmt.Printf("收到消息：%s\n", message)
+	}
 }
