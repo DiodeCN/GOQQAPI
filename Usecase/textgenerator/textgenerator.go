0d57fe7f4116fad2788fe067480702d249b0ff53
DiodeCN <1069523953@qq.com>
2023-05-09 22:15:54 +0800

    vi

--- Usecase/textgenerator/textgenerator.go
+++ Usecase/textgenerator/textgenerator.go
@@ -3,8 +3,30 @@ package textgenerator
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"sync"
 	"time"
+
+	"gopkg.in/ini.v1"
 )
+
+var (
+	triggeredQQs *ini.File
+	mu           sync.Mutex
+)
+
+func init() {
+	var err error
+	triggeredQQs, err = ini.Load("triggeredQQs.ini")
+	if err != nil {
+		if os.IsNotExist(err) {
+			triggeredQQs = ini.Empty()
+			triggeredQQs.SaveTo("triggeredQQs.ini")
+		} else {
+			panic(err)
+		}
+	}
+}
 
 func GenerateAbility(qq int64) string {
 	seed := time.Now().UnixNano() + qq
@@ -14,6 +36,20 @@ func GenerateAbility(qq int64) string {
 	B := (randomNumber % 49) / 7
 	C := randomNumber % 7
 
-	text := fmt.Sprintf("[CQ:at,qq=%d]%d%d%d", qq, A, B, C)
-	return text
+	text := fmt.Sprintf("%d%d%d", A, B, C)
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	section := triggeredQQs.Section("Triggered")
+	key, err := section.GetKey(fmt.Sprintf("%d", qq))
+	if err == nil {
+		text = "111" + text
+	} else {
+		section.NewKey(fmt.Sprintf("%d", qq), "")
+		triggeredQQs.SaveTo("triggeredQQs.ini")
+		text = "222" + text
+	}
+
+	return fmt.Sprintf("[CQ:at,qq=%d]%s", qq, text)
 }
