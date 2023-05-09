e1f79e6054c602ccdf13ebac62c01b3bf7611886
DiodeCN <1069523953@qq.com>
2023-05-09 21:42:53 +0800

    2

---
+++ Usecase/textgenerator/textgenerator.go
@@ -0,0 +1,19 @@
+package textgenerator
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func GenerateText(qq int64) string {
+	seed := time.Now().UnixNano() + qq
+	rand.Seed(seed)
+	randomNumber := rand.Intn(7*7*7) // 7的三次方
+	A := randomNumber / 49
+	B := (randomNumber % 49) / 7
+	C := randomNumber % 7
+
+	text := fmt.Sprintf("[CQ:at,qq=%d]%d%d%d", qq, A, B, C)
+	return text
+}
