0e52264fa96e0ad8a1e3dd3f7f88272a1b7a5e8f
DiodeCN <1069523953@qq.com>
2023-05-09 16:24:27 +0800

    11

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
     "encoding/json"
 )
 
+func generateText(qq int64) string {
+	seed := time.Now().UnixNano() + qq
+	rand.Seed(seed)
+	randomNumber := rand.Intn(7*7*7) // 7的三次方
+	A := randomNumber / 49
+	B := (randomNumber % 49) / 7
+	C := randomNumber % 7
+
+	text := fmt.Sprintf("[CQ:at,qq=%d]%d%d%d", qq, A, B, C)
+	return text
+}
+
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
     // 将HTTP连接升级为WebSocket
