93d090374c9ad1c8eeac593cb61ff5f47c42c8e6
DiodeCN <1069523953@qq.com>
2023-05-10 22:12:43 +0800

    ？

--- Usecase/textgenerator/textgenerator.go
+++ Usecase/textgenerator/textgenerator.go
@@ -75,7 +75,7 @@ func GenerateAbility(qq int64) string {
 
 	text, err := ReadIni(qq, "Ability.ini")
 	if err == nil && text != "" {
-		return fmt.Sprintf("主人今天已经查过啦，[\r\n]" + "[CQ:at,qq=%d]%s", qq, text)
+		return fmt.Sprintf("主人今天已经查过啦，\r\n" + "[CQ:at,qq=%d]%s", qq, text)
 	}
 
 	text = fmt.Sprintf("%d%d%d", A, B, C)
@@ -102,7 +102,7 @@ func GenerateMilitary(qq int64) string {
 
 	text, err := ReadIni(qq, "Military.ini")
 	if err == nil && text != "" {
-		return fmt.Sprintf("主人今天已经查过啦，[\r\n]" +"[CQ:at,qq=%d]%s", qq, text)
+		return fmt.Sprintf("主人今天已经查过啦，\r\n" +"[CQ:at,qq=%d]%s", qq, text)
 	}
 
 	text = fmt.Sprintf("%d%d%d%d", A, B, C, D)
